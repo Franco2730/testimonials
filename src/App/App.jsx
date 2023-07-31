e7d4263ddb7b6540ed77d2df960ac5fdb8accf9e
franco2730 <franco.rosales2130@gmail.com>
2023-07-31 08:56:49 -0300

    javasrock

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='title1'>Testimonials from Glam Seniors about programming</h1>
+    </>
+  )
+}
+
+export default App
